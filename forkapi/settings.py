b9ffedf8e4b11597a1f9099c4d7cd13214850ff0
mikebgrep <72438723+mikebgrep@users.noreply.github.com>
2024-11-02 20:11:48 +0200

    Update settings.py
    Return debug to False

--- forkapi/settings.py
+++ forkapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 # Application definition
