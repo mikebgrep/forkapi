6e0aad9c5c4d4646ca37f87ea6372cd5dd960b20
mikebgrep <mike2016consult@abv.bg>
2024-11-06 15:51:06 +0200

    implement test for auth, tag, category create and update,  start with recipes add two test files

--- tests/forkapi_tests.py
+++ tests/forkapi_tests.py
@@ -1,17 +1,15 @@
-import os
-import uuid
+import os, uuid, random, pytest
 
-import pytest
 from authentication.models import User
-from recipe.models import Category
+from recipe.models import Category, Tag, Recipe
+from rest_framework.authtoken.models import Token
 from dotenv import load_dotenv
 from rest_framework.test import APIClient
-
 
 load_dotenv()
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="function")
 def api_client():
     client = APIClient()
     client.credentials(HTTP_X_AUTH_HEADER=os.getenv('X_AUTH_HEADER'))
@@ -20,24 +18,40 @@ def api_client():
 
 
 @pytest.mark.django_db
-def test_create_admin_user(api_client):
-
+def create_user(api_client):
     admin_user_data = {
-        "username": "admin",
+        "username": f"admin-{random.uniform(0, 10000)}",
         "password": "test123",
         "is_superuser": True
     }
+
     response = api_client.post("/api/auth/signup", admin_user_data, format="json")
     assert response.status_code == 201
-
     admin_user = User.objects.get(username=admin_user_data['username'])
 
-    assert admin_user.username == admin_user_data['username']
-    assert admin_user.is_superuser == True
+    return admin_user, admin_user_data
 
 
 @pytest.mark.django_db
-def test_create_category(api_client):
+def get_token_and_admin_user(api_client):
+    admin_user, admin_user_data = create_user(api_client)
+    response = api_client.post("/api/auth/token", admin_user_data, format="json")
+
+    assert response.status_code == 200
+    token = Token.objects.get(user=admin_user)
+
+    assert token is not None
+
+    return token, admin_user, admin_user_data
+
+
+def add_access_token_header(api_client):
+    token, admin_user, admin_user_data = get_token_and_admin_user(api_client)
+    api_client.credentials(HTTP_AUTHORIZATION=f"Token {token}")
+
+
+@pytest.mark.django_db
+def create_category(api_client):
     category_data = {
         "name": f"Category-{uuid.uuid4()}"
     }
@@ -47,5 +61,173 @@ def test_create_category(api_client):
 
     category = Category.objects.get(name=category_data['name'])
 
+    return category, category_data
+
+
+@pytest.mark.django_db
+def create_tag(api_client):
+    tag_data = {
+        "name": f"Tag-{uuid.uuid4()}"
+    }
+
+    response = api_client.post("/api/recipe/tag/add", tag_data, format="json")
+    assert response.status_code == 201
+
+    tag = Tag.objects.get(name=tag_data['name'])
+
+    return tag, tag_data
+
+
+@pytest.mark.django_db
+def create_recipe(api_client):
+    tag, tag_data = create_tag(api_client)
+    category, category_data = create_category(api_client)
+
+    recipe_data = {
+        "name": f"Recipe-{uuid.uuid4()}",
+        "serves": int(random.uniform(0,20)),
+        "category": category.pk,
+        "tag": tag.pk,
+        "prep_time": int(random.uniform(1, 60)),
+        "image": open("tests/uploads/upload-image.png", 'rb'),
+        "video": open("tests/uploads/upload-video.mp4", 'rb')
+    }
+
+    response = api_client.post("/api/recipe/", recipe_data, format="multipart")
+    assert response.status_code == 201
+    recipe = Recipe.objects.get(name=recipe_data['name'])
+
+    return recipe, recipe_data
+
+
+@pytest.mark.django_db
+def test_create_admin_user(api_client):
+    admin_user, admin_user_data = create_user(api_client)
+
+    assert admin_user.username == admin_user_data['username']
+    assert admin_user.is_superuser == True
+
+
+@pytest.mark.django_db
+def test_login_with_non_existing_user(api_client):
+    login_data = {
+        "username": "non_existing_user",
+        "password": f"password-{random.uniform(1000, 100000)}"
+    }
+    response = api_client.post("/api/auth/token", login_data, format="json")
+    assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test_login_with_wrong_auth_header(api_client):
+    admin_user, admin_user_data = create_user(api_client)
+    api_client.credentials(HTTP_X_AUTH_HEADER=None)
+
+    response = api_client.post("/api/auth/token", admin_user_data, format="json")
+    assert response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_create_category(api_client):
+    add_access_token_header(api_client)
+    category, category_data = create_category(api_client)
     assert category.name == category_data['name']
-    return category
+
+
+@pytest.mark.django_db
+def test_create_category_invalid_type(api_client):
+    category_data = {
+        "name": False
+    }
+
+    add_access_token_header(api_client)
+    response = api_client.post("/api/recipe/category/add", category_data, format="json")
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_create_category_without_token(api_client):
+    category_data = {
+        "name": False
+    }
+
+    response = api_client.post("/api/recipe/category/add", category_data, format="json")
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_update_category(api_client):
+    add_access_token_header(api_client)
+    category, category_data = create_category(api_client)
+
+    update_data = {
+        "name": f"Category-{uuid.uuid4()}"
+    }
+
+    response = api_client.put(f"/api/recipe/category/{category.pk}", update_data, format="json")
+
+    assert response.status_code == 200
+
+    updated_category = Category.objects.get(pk=category.pk)
+    assert updated_category.name == update_data['name']
+
+
+@pytest.mark.django_db
+def test_create_tag(api_client):
+    add_access_token_header(api_client)
+    tag, tag_data = create_tag(api_client)
+
+    assert tag.name == tag_data['name']
+
+
+@pytest.mark.django_db
+def test_create_tag_incorrect_data(api_client):
+    tag_data = {
+        "name": False
+    }
+
+    add_access_token_header(api_client)
+    response = api_client.post("/api/recipe/tag/add", tag_data, format="json")
+    assert response.status_code == 400
+
+
+@pytest.mark.django_db
+def test_create_tag_without_token(api_client):
+    tag_data = {
+        "name": f"Tag-{uuid.uuid4()}"
+    }
+
+    response = api_client.post("/api/recipe/tag/add", tag_data, format="json")
+    assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_update_tag_name(api_client):
+    add_access_token_header(api_client)
+    tag, tag_data = create_tag(api_client)
+
+    update_tag_data = {
+        "name": f"Tag-{uuid.uuid4()}"
+    }
+
+    response = api_client.put(f"/api/recipe/tag/{tag.pk}", update_tag_data, format="json")
+    assert response.status_code == 200
+
+    tag = Tag.objects.get(pk=tag.pk)
+    assert tag.name == update_tag_data['name']
+
+
+@pytest.mark.django_db
+def test_create_recipe(api_client):
+    add_access_token_header(api_client)
+    recipe, recipe_data = create_recipe(api_client)
+
+    assert recipe.name == recipe_data['name']
+    assert recipe.serves == recipe_data['serves']
+    assert recipe.is_favorite == False
+    assert recipe.tag.pk == recipe_data['tag']
+    assert recipe.category.pk == recipe_data['category']
+    assert recipe.created_at is not None
+    assert recipe.image is not None
+    assert recipe.video is not None
+    assert recipe.prep_time == recipe_data['prep_time']
