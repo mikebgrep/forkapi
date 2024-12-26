5b2530e9ad6f14bd1ec7dc5c35bf04ed2e4ea1e2
mikebgrep <72438723+mikebgrep@users.noreply.github.com.>
2024-12-26 13:36:55 +0200

    bump quantity to 20 chars

--- forkapi/recipe/models.py
+++ forkapi/recipe/models.py
@@ -72,7 +72,7 @@ class Recipe(models.Model):
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=120)
-    quantity = models.CharField(max_length=5)
+    quantity = models.CharField(max_length=20)
     metric = models.CharField(max_length=10)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
 
