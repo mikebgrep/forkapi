a9d8c2ca35201ec5e552653b83957740940ea045
mikebgrep <72438723+mikebgrep@users.noreply.github.com.>
2024-12-30 15:25:05 +0200

    delete not neded conversion for minuts

--- forkapi/recipe/util.py
+++ forkapi/recipe/util.py
@@ -7,7 +7,4 @@ def calculate_recipe_total_time(total: int) -> str:
 
     hours = total // 60
     minutes = total % 60
-    fractional_hours = minutes / 60
-    total_hours = hours + fractional_hours
-    print(total_hours)
     return f"{hours}.{minutes:02}"
