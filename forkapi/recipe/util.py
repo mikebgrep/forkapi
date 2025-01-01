33b2ae663d781cba706772c5a29251ac9d182d16
mikebgrep <72438723+mikebgrep@users.noreply.github.com.>
2025-01-01 14:41:24 +0200

    fix total < 60 return value in util

--- forkapi/recipe/util.py
+++ forkapi/recipe/util.py
@@ -3,7 +3,7 @@
 
 def calculate_recipe_total_time(total: int) -> str:
     if total < 60:
-        return f"{total:.2f}"
+        return f"{total / 100:.2f}"
 
     hours = total // 60
     minutes = total % 60
